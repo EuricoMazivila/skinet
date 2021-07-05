2a65e27446439df7501e205985235332278cc953
EuricoMazivila <50259548+EuricoMazivila@users.noreply.github.com>
2021-07-05 09:16:01 +0200

    Initial Commit

---
+++ Persistence/StoreContext.cs
@@ -0,0 +1,14 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
