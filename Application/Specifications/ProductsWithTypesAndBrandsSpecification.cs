7e15b4934ada2fd8ddb9760415e0d3c21abac6fd
EuricoMazivila <50259548+EuricoMazivila@users.noreply.github.com>
2021-07-08 15:53:05 +0200

    End of section 4

---
+++ Application/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,20 @@
+using Domain;
+
+namespace Application.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x=>x.ProductBrand);
+            AddInclude(x=>x.ProductType);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id)
+            : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductBrand);
+            AddInclude(x => x.ProductType);   
+        }
+    }
+}
